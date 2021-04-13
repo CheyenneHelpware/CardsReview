4fb6e8a6fee97945a4cc949c3f07a720865beeaf
CheyenneHelpware <cheyennehinckley04@gmail.com>
2021-04-13 08:43:48 -0400

    first working version using array only

--- CardsReview/ViewController.swift
+++ CardsReview/ViewController.swift
@@ -1,19 +1,33 @@
-//
-//  ViewController.swift
-//  CardsReview
-//
-//  Created by Cheyenne on 4/13/21.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITabBarDelegate {
+   
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+       
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return defaultDeck.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.textLabel?.text = "\(defaultDeck[indexPath.row])"
+        cell.imageView?.image = UIImage(named: defaultDeck[indexPath.row])
+        return cell
+    }
+    func tableView( tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(175)
+    }
+    
+
 
 
 }
+
 
---
+++ CardsTableView.swift
@@ -0,0 +1,8 @@
+//
+//  CardsTableView.swift
+//  CardsReview
+//
+//  Created by Cheyenne on 4/13/21.
+//
+
+import Foundation
